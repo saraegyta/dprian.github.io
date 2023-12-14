3b43f4e63c821b77ece0f442d642f65cea1026d6
saraegyta <saraegytakamelia@gmail.com>
2023-12-14 11:36:59 +0700

    commit

---
+++ aksi.php
@@ -0,0 +1,13 @@
+<?php
+
+$nama = $_POST["name"];
+$email = $_POST["email"];
+$phonenumber = $_POST["phonenumber"];
+
+echo "<h2> Form data yang diambil : </h2>";
+echo "<p> nama : $nama </p>";
+echo "<p> email : $email </p>";
+echo "<p> phonenumber : $phonenumber </p>";
+
+
+?>
---
+++ form.php
@@ -0,0 +1,31 @@
+<div class="container contact-form">
+            <div class="contact-image">
+            </div cllas="row justify-content-center">
+
+            </div>
+            <form action="aksi.php" method="post">
+            <div class="container text-center">
+                <h3>Contact Us</h3>
+            </div>
+               <div class="row justify-content-center">
+                    <div class="col-md-5">
+                        <div class="form-group">
+                        <label for="name">Name:</label>
+                            <input type="text" name="name" class="form-control" placeholder="Your Name *" value="" />
+                        </div>
+                        <div class="form-group">
+                        <label for="name">Email:</label>
+                            <input type="email" name="email" class="form-control" placeholder="Your Email *" value="" />
+                        </div>
+                        <div class="form-group">
+                        <label for="name">Phone Number:</label>
+                            <input type="number" name="phonenumber" class="form-control" placeholder="Your Phone Number *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="btnSubmit" class="btnContact" value="Submit" />
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+</div>
